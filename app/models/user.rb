5b1fed662c36a895e114bc07bf9f8e2c650d499e
mun1029 <ring01061995@gmail.com>
2021-07-24 21:42:50 +0900

    中間の作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :image
+  has_many :room_users
+  has_many :rooms, through: :room_users
 
   validates :nickname, presence: true
   
