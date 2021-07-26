699fb23ea3bc9e5d0ae3b7fcc7cb28a725dd174c
mun1029 <ring01061995@gmail.com>
2021-07-26 15:57:04 +0900

    リストのルート再設定

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -9,9 +9,17 @@ class ListsController < ApplicationController
   end
 
   def create
-    @list = List.create(list_params)
-
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to lists_path
+    else
+      render :new
+    end
   end
+
+  def edit
+  end
+
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to:'users#index'
-  resources :users do
-    resources :lists, only: [:new, :create, :index]
-    resources :rooms, only: [:new, :create]
-  end
+  resources :users
+  resources :lists, only: [:new, :create, :index, :edit, :update]
+  resources :rooms, only: [:new, :create]
+  
 end
